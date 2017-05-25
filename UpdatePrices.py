7768c51dcb9424b7b567e4e134509e2720680141
drewtuley <drewtuley@gmail.com>
2017-05-25 21:09:03 +0100

    minor format tweaks

--- UpdatePrices.py
+++ UpdatePrices.py
@@ -1,26 +1,29 @@
 import ConfigParser
+import logging
 import sqlite3
 from datetime import datetime
+
 import requests
-import logging
+
 
 def fixup(vals):
     ret = []
     for v in vals:
         if v == '-':
-            v=-1
+            v = -1
         ret.append(v)
 
     return ret
+
 
 if __name__ == '__main__':
     config = ConfigParser.SafeConfigParser()
     config.read('portfolio.props')
 
-
-    logging.basicConfig(format=config.get('logging','log_format'), \
-        filename=config.get('logging','filename').format(dir=config.get('logging','dir'),dt=str(datetime.now())[:10]), \
-        level=logging.DEBUG)
+    logging.basicConfig(format=config.get('logging', 'log_format'), \
+                        filename=config.get('logging', 'filename').format(dir=config.get('logging', 'dir'),
+                                                                          dt=str(datetime.now())[:10]), \
+                        level=logging.DEBUG)
     logging.captureWarnings(True)
 
     iso_date_format = config.get('database', 'iso_date_format')
@@ -32,9 +35,9 @@ if __name__ == '__main__':
         crsr = conn.cursor()
 
         # get max price date for each portfolio
-        today=datetime.now()
-        api_date_format = config.get('google.finance','url_date_format')
-        price_date_format = config.get('google.finance','price_date_format')
+        today = datetime.now()
+        api_date_format = config.get('google.finance', 'url_date_format')
+        price_date_format = config.get('google.finance', 'price_date_format')
 
         api_url = config.get('google.finance', 'api')
         formatted_end_date = api_date_format.format(dt=today)
@@ -59,9 +62,10 @@ if __name__ == '__main__':
                         if price_date > end_date:
                             l_open, l_high, l_low, l_close = fixup([l_open, l_high, l_low, l_close])
                             iso_price_date = datetime.strftime(price_date, iso_date_format)
-                            sql = insert_sql.format(google_cid=google_cid, iso_date=iso_price_date, open=l_open, high=l_high, low=l_low, close=l_close, volume=l_volume)
+                            sql = insert_sql.format(google_cid=google_cid, iso_date=iso_price_date, open=l_open,
+                                                    high=l_high, low=l_low, close=l_close, volume=l_volume)
                             logging.debug('insert sql: {}'.format(sql))
-                            #print('{} {}'.format(l_date, price_date))
+                            # print('{} {}'.format(l_date, price_date))
                             conn.execute(sql)
                         else:
-                            logging.info('skipping '+l)
+                            logging.info('skipping ' + l)
