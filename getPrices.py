54fc484b7bab723afda7a066b5a052ba06513f55
drewtuley <drewtuley@gmail.com>
2017-05-25 08:47:43 +0100

    PyCharm reformat

--- getPrices.py
+++ getPrices.py
@@ -1,9 +1,9 @@
 import requests
 from datetime import datetime
 
-API_URL='http://www.google.co.uk/finance/historical?cid={}&startdate=Jan+1,+2014&enddate={}&output=csv'
+API_URL = 'http://www.google.co.uk/finance/historical?cid={}&startdate=Jan+1,+2014&enddate={}&output=csv'
 
-today=datetime.now().strftime('%b %d,%Y')
+today = datetime.now().strftime('%b %d,%Y')
 with open('portfolio_meta.txt') as fd:
     for line in fd:
         name, ticker, cid = line.strip().split(',')
@@ -11,11 +11,13 @@ with open('portfolio_meta.txt') as fd:
         geturl = API_URL.format(str(cid), today)
         print(geturl)
         response = requests.get(geturl)
-        if len(response.text) >0 :
-            fname='{}.csv'.format(name)
-            with open(fname,'w') as fd:
+        if len(response.text) > 0:
+            fname = '{}.csv'.format(name)
+            with open(fname, 'w') as fd:
                 for line in response.text.split('\n'):
                     if len(line) > 0:
                         dt, op, hi, lo, close, volume = line.split(',')
                         if dt != 'Date':
-                            fd.write ('insert into price select {},"{}",{},{},{},{},{};\n'.format(cid, dt, op, hi, lo, close, volume))
+                            fd.write(
+                                'insert into price select {},"{}",{},{},{},{},{};\n'.format(cid, dt, op, hi, lo, close,
+                                                                                            volume))
